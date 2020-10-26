4c19288cd1f3fe2330af74dd27dc97901322275b
Konstantin Selivanov <mail@selivanovk.com>
2020-10-25 20:38:29 -0400

    rows and bunner implemented

--- src/Row.js
+++ src/Row.js
@@ -1,27 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
-function Row({ title, fetchURL }) {
+const base_url = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  
- // A snippet of code which runs based on a specific condition/variable
+
+  // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchURL]);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
 
-      {/* container  -> posters*/}
+      <div className="row__posters">
+        {/* several row__poster(s) */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
 
+      {/* container  -> posters*/}
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
